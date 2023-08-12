ce0035c8291597a2011ac581e7f7f7ce88f43cb4
m.d.rezaei <m.d.rezaei.official@gmail.com>
2023-08-12 06:46:23 +0330

    First Commit

---
+++ PartialClass.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11_PartialClass
+{
+    partial class PartialClass//کلاس های پارشیال به معنای تکه تکه هستند که موجب میشود ما از نام یک کلاس بار ها استفاده کنیم و کلاسی با یک نام را چندین جا ایجاد کنیم و در ان کلاس های پارشیال هم نام متد ها و پراپرتی ها و چیز های دیگری با نام های <مختلف> ایجاد و استفاده کنیم. کار برد این کلاس ها بیشتر در کارهای تیمی است. در این کلاس ها نمیتوان از نام های تکراری برای متد ها و پراپرتی ها استفاده کرد و یا دیگر اجزای درون بلاک کلاس ها چون بعد کامپایل و بیلد برنامه کلاس های پارشیال هم نام با هم ادغام و مرج میشوند و میدانیم که نمیشود در یک بلاک از نام های تکراری استفاده کرد.
+    {
+        public void Sum()
+        {
+            Console.WriteLine("Enter Number");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Number");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(a+b);
+
+        }
+    }
+
+    partial class PartialClass
+    {
+        public void Print()
+        {
+            Console.WriteLine("PRINT");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConsoleApp11_PartialClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PartialClass PC = new PartialClass();//در اینجا <پی سی> یک نمونه از کلاس پارشیال <پارشیالکلاس> است
+            PC.Sum();//سام در یک کلاس پارشیال است
+            Console.WriteLine(PC.Minius(10, 5)); // ماینس در کلاس پارشیال دیگری است
+            PC.Print();//پرینت نیز از یک کلاس پارشیال دیگر هم نام با دو کلاس دیگر هست. که همگی با <پی سی> که نمونه ای از کلاس های هم نامشان است قابل دسترسی اند
+        }
+    }
+}
